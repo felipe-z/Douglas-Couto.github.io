510658066f0ffe19887027bfb7ffa594fbc19240
Douglas-Couto <66683439+Douglas-Couto@users.noreply.github.com>
2021-02-08 10:47:31 -0300

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -12,7 +12,7 @@ let render_projects = (slug) => {
     let projects_obj = [
         {
             image: 'assets/images/mentors.jpg',
-            link: 'https://docs.google.com/gview?url=https://github.com/Douglas-Couto/Douglas-Couto.github.io/blob/master/PDFs/CVDouglasCouto.pdf &embedded=true ',
+            link: 'https://docs.google.com/gview?url=https://Douglas-Couto.github.io/blob/master/PDFs/CVDouglasCouto.pdf &embedded=true ',
             title: 'TESTE',
             demo: 'https://mporter.co',
             technologies: ['Flask', 'Celery', 'Python'],
